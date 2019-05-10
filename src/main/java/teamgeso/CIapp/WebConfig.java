2fb162e4aa10a6d83d96364dbe779fd052a0a850
basil <basilossandon@gmail.com>
2019-05-10 07:19:20 -0400

    test

---
+++ src/main/java/teamgeso/CIapp/WebConfig.java
@@ -0,0 +1,11 @@
+package teamgeso.CIapp;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+        registry.allowedMethods("*");
+    }
+}
