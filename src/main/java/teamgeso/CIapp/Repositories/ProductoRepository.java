0e52ad9a1b16cace2b7d33b730794cff44ba0961
eliass1997ok <gabriel.gaete.l@usach.cl>
2019-05-10 05:57:49 -0400

    fix.

--- src/main/java/teamgeso/CIapp/Repositories/ProductoRepository.java
+++ src/main/java/teamgeso/CIapp/Repositories/ProductoRepository.java
@@ -3,6 +3,6 @@ package teamgeso.CIapp.Repositories;
 import teamgeso.CIapp.Models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    Producto findProductoById(Long id);
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    Producto findProductoById(Integer id);
 }
