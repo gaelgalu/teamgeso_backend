a05d971eac05850b2944b5372b618e891ee0efd3
eliass1997ok <gabriel.gaete.l@usach.cl>
2019-05-10 01:21:24 -0400

    this is an example of happiness.

--- src/main/java/teamgeso/CIapp/Controllers/ProductoController.java
+++ src/main/java/teamgeso/CIapp/Controllers/ProductoController.java
@@ -27,7 +27,7 @@ public class ProductoController{
         return productRepository.findAll();
     }
 
-    //This is an example of a comment
+    //This is an example of a comments
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public Producto getProductById(@PathVariable Long id) {
