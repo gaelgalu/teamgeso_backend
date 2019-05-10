78016f1560f556e32d768324287c449db373f0a5
l-pizarro <leandro.pizarro@usach.cl>
2019-05-10 10:27:36 -0400

    Final test before presentation

--- src/main/java/teamgeso/CIapp/Controllers/ProductoController.java
+++ src/main/java/teamgeso/CIapp/Controllers/ProductoController.java
@@ -6,24 +6,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
-@RequestMapping("/productos")
-@CrossOrigin(origins = "*")
 public class ProductoController{
 	@Autowired
-    private ProductoRepository productRepository;
+	private ProductoRepository productRepository;
 
-    @RequestMapping(method = RequestMethod.GET)
+
+    @CrossOrigin(origins = "http://157.230.12.110:8082/")
+    @RequestMapping("/productos")
     @ResponseBody
-    public List<Producto> getAllUsers() {
+    public List<Producto> getAllProducts() {
         return productRepository.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public Producto create(@RequestBody Producto resource) {
+    public Producto getProductById(@PathVariable Integer id) {
+        return productRepository.findProductoById(id);
+    }
+
+    @RequestMapping(value = "/nuevo", method = RequestMethod.POST)
+    @ResponseBody
+    public Producto create(@RequestBody Producto resource){
         return productRepository.save(resource);
     }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/borrar/{id}")
+        public String deleteProduct(@PathVariable Integer id) {
+        String deletedName = this.getProductById(id).getProductName();
+        productRepository.deleteById(id);
+        return deletedName;
+    }
+    @PutMapping("/editar/{id}")
+    public HttpStatus updateProduct(@RequestBody Producto product, @PathVariable Integer id) {
+        Optional<Producto> productOptional = productRepository.findById(id);
+        if (!productOptional.isPresent())
+            return HttpStatus.NOT_FOUND;
+        product.setId(id);
+        
+        productRepository.save(product);
+
+        return HttpStatus.OK;
+    }
+
 }
