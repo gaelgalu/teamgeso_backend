0366e984cd96b5a62e32244b8c90fd18ef743dbf
l-pizarro <leandro.pizarro@usach.cl>
2019-05-10 10:15:26 -0400

    ProductoService update

--- src/main/java/teamgeso/CIapp/Controllers/ProductoController.java
+++ src/main/java/teamgeso/CIapp/Controllers/ProductoController.java
@@ -5,54 +5,25 @@ import teamgeso.CIapp.Repositories.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.text.ParseException;
-
-
 
 @RestController
+@RequestMapping("/productos")
+@CrossOrigin(origins = "*")
 public class ProductoController{
 	@Autowired
-	private ProductoRepository productRepository;
+    private ProductoRepository productRepository;
 
-
-    @CrossOrigin(origins = "*")
-    @RequestMapping("/productos")
+    @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public List<Producto> getAllProducts() {
+    public List<Producto> getAllUsers() {
         return productRepository.findAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public Producto getProductById(@PathVariable Integer id) {
-        return productRepository.findProductoById(id);
-    }
-
-    @RequestMapping(value = "/nuevo", method = RequestMethod.POST)
-    @ResponseBody
-    public Producto create(@RequestBody Producto resource){
+    public Producto create(@RequestBody Producto resource) {
         return productRepository.save(resource);
     }
-
-    @RequestMapping( value = "/borrar/{id}", method = RequestMethod.DELETE)
-        public String deleteProduct(@PathVariable Integer id) {
-        String deletedName = this.getProductById(id).getProductName();
-        productRepository.deleteById(id);
-        return deletedName;
-    }
-    @PutMapping("/editar/{id}")
-    public HttpStatus updateProduct(@RequestBody Producto product, @PathVariable Integer id) {
-        Optional<Producto> productOptional = productRepository.findById(id);
-        if (!productOptional.isPresent())
-            return HttpStatus.NOT_FOUND;
-        product.setId(id);
-        
-        productRepository.save(product);
-
-        return HttpStatus.OK;
-    }
-
 }
