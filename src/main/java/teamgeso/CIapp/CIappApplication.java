7180df38a377375f4f73d60a1d21e9ca22b75ecd
basil <basilossandon@gmail.com>
2019-05-21 05:15:31 -0400

    v1.0.1.2 CORS addCorsMappings typo

--- src/main/java/teamgeso/CIapp/CIappApplication.java
+++ src/main/java/teamgeso/CIapp/CIappApplication.java
@@ -29,9 +29,9 @@ public class CIappApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://157.230.12.110:8082");
-            .allowedHeaders("Access-Control-Allow-Origin", "*")
-            .allowedHeaders("Access-Control-Allow-Headers", "Content-Type,x-requested-with").maxAge(20000)
-            .allowCredentials(false)
+            .allowedHeaders("Access-Control-Allow-Origin", "*");
+            .allowedHeaders("Access-Control-Allow-Headers", "Content-Type,x-requested-with").maxAge(20000);
+            .allowCredentials(false);
             .allowedMethods("DELETE");
             }
         };
